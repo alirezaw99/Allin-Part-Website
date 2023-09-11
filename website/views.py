a4fa7b9f0d44ef3ef04e1a508fbff00539caa486
Alien <128927380+alirezaw99@users.noreply.github.com>
2023-09-11 16:20:06 +0330

    First App created
    First app created with two views,
    home_view and contact_view

---
+++ website/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home_view(request):
+    return HttpResponse('<h1>This is Home Page</h1>')
+
+def contact_view(request):
+    return HttpResponse('<h1>This is Contact Page</h1>')
