4912c59e6e9707a54ff3141bff11bc68e28f2147
Alien <128927380+alirezaw99@users.noreply.github.com>
2023-09-12 10:28:56 +0330

    templates folder moved
    templates folder moved to the base directory
    directory added to the setting file

--- website/views.py
+++ website/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'Front\index.html')
+    return render(request, 'index.html')
 
 def contact_view(request):
     return HttpResponse('<h1>This is Contact Page</h1>')
