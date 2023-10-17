cb1b482ca2bcfa238f64d41074067f7941afe180
Alien <128927380+alirezaw99@users.noreply.github.com>
2023-10-17 09:42:55 +0330

    models added, admin created
    some models are added and registered
    also admin panel created

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Brands,Auto_Washing_Machine_models, Dishwasher_models, Twin_Washing_Machine_models, Vacuum_Cleaner_models, Refrigerator_models, Accessory_models
 
 # Register your models here.
+admin.site.register(Brands)
+admin.site.register(Auto_Washing_Machine_models)
+admin.site.register(Dishwasher_models)
+admin.site.register(Twin_Washing_Machine_models)
+admin.site.register(Vacuum_Cleaner_models)
+admin.site.register(Refrigerator_models)
+admin.site.register(Accessory_models)
