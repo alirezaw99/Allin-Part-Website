849e8746c6547f0353c3d4fb3c29b35347aae345
Alien <128927380+alirezaw99@users.noreply.github.com>
2023-11-26 14:56:16 +0330

    urls and views
    url path and views are added to the project

--- main/urls.py
+++ main/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , include('main.urls')),
+    path('' ,home_view),
+    path('contact_us', contact_view),
+    path('about_us', about_view)
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home_view(request):
+    return render(request, './home.html')
+
+def contact_view(request):
+    return render(request, './contact_us.html')
+
+def about_view(request):
+    return render(request, './about_us.html')
+
