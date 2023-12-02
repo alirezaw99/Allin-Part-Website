cab86194d5063e08310715e81edea561fc31f8c8
Alien <128927380+alirezaw99@users.noreply.github.com>
2023-12-02 14:24:26 +0330

    Superuser and models added
    Superuser is created and 'brand' model has defined and registered.

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
-from .models import Brands,Auto_Washing_Machine_models, Dishwasher_models, Twin_Washing_Machine_models, Vacuum_Cleaner_models, Refrigerator_models, Accessory_models
+from .models import *
 
 # Register your models here.
-admin.site.register(Brands)
-admin.site.register(Auto_Washing_Machine_models)
-admin.site.register(Dishwasher_models)
-admin.site.register(Twin_Washing_Machine_models)
-admin.site.register(Vacuum_Cleaner_models)
-admin.site.register(Refrigerator_models)
-admin.site.register(Accessory_models)
+admin.site.register(Brand)
--- main/models.py
+++ main/models.py
@@ -1,51 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Brands(models.Model):
-    name = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.name
-    
-class Auto_Washing_Machine_models(models.Model):
-    model_brand = models.OneToOneField(Brands, on_delete=models.SET_NULL, primary_key=False, null=True)
-    model_name = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.model_name
-    
-class Dishwasher_models(models.Model):
-    model_brand = models.OneToOneField(Brands, on_delete=models.SET_NULL, primary_key=False, null=True)
-    model_name = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.model_name
-    
-class Twin_Washing_Machine_models(models.Model):
-    model_brand = models.OneToOneField(Brands, on_delete=models.SET_NULL, primary_key=False, null=True)
-    model_name = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.model_name
-    
-class Vacuum_Cleaner_models(models.Model):
-    model_brand = models.OneToOneField(Brands, on_delete=models.SET_NULL, primary_key=False, null=True)
-    model_name = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.model_name
-    
-class Refrigerator_models(models.Model):
-    model_brand = models.OneToOneField(Brands, on_delete=models.SET_NULL, primary_key=False, null=True)
-    model_name = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.model_name
-    
-class Accessory_models(models.Model):
-    model_brand = models.OneToOneField(Brands, on_delete=models.SET_NULL, primary_key=False, null=True)
-    model_name = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.model_name
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_now_add=True)
+    edited_date = models.DateTimeField(auto_now=True)
     
