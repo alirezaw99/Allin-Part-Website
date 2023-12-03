60889e27ce5edb8467a2f5c546d4c32c6fa028f8
Alien <128927380+alirezaw99@users.noreply.github.com>
2023-12-03 09:46:13 +0330

    first models
    first models are created and registered on admin.py

--- main/admin.py
+++ main/admin.py
@@ -3,3 +3,6 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Brand)
+admin.site.register(Tag)
+admin.site.register(Auto_Washing_Machine_Model)
+admin.site.register(Auto_Washing_Machine_Part)
--- main/models.py
+++ main/models.py
@@ -6,3 +6,40 @@ class Brand(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     edited_date = models.DateTimeField(auto_now=True)
     
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_now_add=True)
+    edited_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+  
+class Auto_Washing_Machine_Model(models.Model):
+    model_brand = models.OneToOneField(Brand, on_delete=models.SET_NULL, null=True)
+    model_name = models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_now_add=True)
+    edited_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.model_brand} - {self.model_name}"
+    
+class Auto_Washing_Machine_Part(models.Model):
+    name = models.CharField(max_length=255)
+    part_number = models.CharField(max_length=15)
+    used_models = models.ForeignKey(Auto_Washing_Machine_Model, on_delete=models.SET_NULL, null=True)
+    price = models.PositiveIntegerField()
+    #picture = models.ImageField()
+    description = models.TextField()
+    in_stock = models.BooleanField(default=False)
+    is_onsale = models.BooleanField(default=False)
+    discounted_price = models.PositiveIntegerField(default=0)
+    tags = models.ForeignKey(Tag, on_delete=models.SET_NULL, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    edited_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
