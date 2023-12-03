f2abab0e1fdd008a8693863cf01b8fc975e1e9a4
Alien <128927380+alirezaw99@users.noreply.github.com>
2023-12-03 13:31:10 +0330

    category templatet
    some minor changes to category template

--- main/views.py
+++ main/views.py
@@ -4,19 +4,20 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_view(request):
-    return render(request, './home.html')
+    return render(request, 'home.html')
 
 def contact_view(request):
-    return render(request, './contact_us.html')
+    return render(request, 'contact_us.html')
 
 def about_view(request):
-    return render(request, './about_us.html')
+    return render(request, 'about_us.html')
 
-def categories_view(request):
-    return render(request, './categories.html')
+def categories_view(request, cat_name):
+    context = {str(cat_name):'sdf'}
+    return render(request, 'categories.html', context)
 
 def parts_list_view(request):
-    return render(request, './parts_list.html')
+    return render(request, 'parts_list.html')
 
 def parts_detail_view(request):
-    return render(request, './part_single.html')
+    return render(request, 'part_single.html')
