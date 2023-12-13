c08f5e5666fa2815338e127ced6966e83aca9b69
Alien <128927380+alirezaw99@users.noreply.github.com>
2023-12-13 10:17:20 +0330

    side filter section
    side filter section implemented

--- main/views.py
+++ main/views.py
@@ -24,17 +24,17 @@ def categories_view(request):
 def parts_list_view(request, slug):
     try:
         cat_name = Category.objects.get(slug=slug)
+        main_cat = Category.objects.get(slug=slug)
         parts = Part.objects.filter(category=cat_name)
         sub_cat = Sub_Category.objects.filter(category=cat_name)
-        brands = Model.objects.filter(category=cat_name)
     except (Category.DoesNotExist, UnboundLocalError ) :
-        cat_name = get_object_or_404(Sub_Category, slug=slug)
-        parts = Part.objects.filter(sub_category=cat_name)
         cat_id = slug.split('-')[-1]
+        cat_name = get_object_or_404(Sub_Category, slug=slug)
+        main_cat = Category.objects.get(pk=cat_id)
+        parts = Part.objects.filter(sub_category=cat_name)
         sub_cat = Sub_Category.objects.filter(category=Category.objects.get(pk=cat_id))
-        brands = Model.objects.filter(category=Category.objects.get(pk=cat_id))
     
-    context = {'cat_name':cat_name, 'parts':parts, 'sub_cats':sub_cat, 'brands':brands}
+    context = {'cat_name':cat_name, 'parts':parts, 'sub_cats':sub_cat, 'main_cat':main_cat}
     
     return render(request, 'parts_list.html', context)
 
