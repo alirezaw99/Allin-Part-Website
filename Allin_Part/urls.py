83d2b0c01db4533419350c56b99c700f6c661e4f
Alien <128927380+alirezaw99@users.noreply.github.com>
2023-12-27 11:36:51 +0330

    robots + update pip

--- Allin_Part/urls.py
+++ Allin_Part/urls.py
@@ -34,7 +34,9 @@ urlpatterns = [
     path('' , include('main.urls')),
     
     path(
-        "sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap",),
+        "sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
+    path('robots.txt', include('robots.urls')),
+
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
