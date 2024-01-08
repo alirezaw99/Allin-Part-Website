4ad2021b6fc7c2c74deaa109aff15ff1ddcf3229
Alien <128927380+alirezaw99@users.noreply.github.com>
2024-01-08 09:46:39 +0330

    settings separated  to dev and prod
    settings separated  to dev and prod
    env added

---
+++ Allin_Part/setting/prod.py
@@ -0,0 +1,43 @@
+from decouple import config
+from Allin_Part.settings import *
+# Initialise environment variables
+
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', cast=bool)
+
+
+ALLOWED_HOSTS = ["*"]
+
+
+#Site ID
+SITE_ID = 2
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "Statics",
+]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = 'media/'
+
+CSRF_COOKIE_SECURE = True
--- Allin_Part/settings.py
+++ Allin_Part/settings.py
@@ -19,13 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r9j2dc%(qlv-6^lxctes04ic7me5h)!!57fwt4cn%7&)d#x!z-'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -44,7 +38,6 @@ INSTALLED_APPS = [
     'main'
 ]
 
-SITE_ID = 2
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -78,17 +71,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Allin_Part.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -120,17 +102,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "Statics",
-]
-
-MEDIA_URL = 'media/'
-MEDIA_ROOT = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
