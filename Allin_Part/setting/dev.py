abcf7787cea21dc55be6243045888a2c0d4cc050
Alien <128927380+alirezaw99@users.noreply.github.com>
2024-01-20 12:05:37 +0330

    few changes

--- Allin_Part/setting/dev.py
+++ Allin_Part/setting/dev.py
@@ -21,6 +21,11 @@ SITE_ID = config('SITE_ID', cast=int, default=1)
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+#     }
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -28,9 +33,17 @@ DATABASES = {
     }
 }
 
+
 # DATABASES = {
-#     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-#     }
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': config('NAME', default='postgre'),
+#        'USER': config('USER', default='postgre'),
+#        'PASSWORD': config('PASSWORD', default='postgre'),
+#        'HOST': config('HOST', default='postgre'),
+#        'PORT': config('PORT', default='5432'),
+#    }
+# }
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
