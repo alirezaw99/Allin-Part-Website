5aa1638810fc13caf1b65c5f6b796ee92d6cb732
Alien <128927380+alirezaw99@users.noreply.github.com>
2024-01-27 11:55:56 +0330

    SEO Optimize

--- Allin_Part/setting/dev.py
+++ Allin_Part/setting/dev.py
@@ -58,4 +58,12 @@ STATICFILES_DIRS = [
 MEDIA_URL = 'media/'
 MEDIA_ROOT = 'media/'
 
-CSRF_COOKIE_SECURE = False
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
