88f0699602846222dc6dae8df70a5a26a40a6d83
Alien <128927380+alirezaw99@users.noreply.github.com>
2024-02-06 13:03:48 +0330

    Home page change
    +top section color changed
    +typing text animation removed
    +allin part writing added with animation

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,7 +1,7 @@
 from .models import Category, Sub_Category
 
 def base_context(request):
-    category = Category.objects.all()
+    category = Category.objects.all().order_by('created_date')
     sub_category = Sub_Category.objects.all()
     
     return {'category':category, 'sub_category':sub_category}
