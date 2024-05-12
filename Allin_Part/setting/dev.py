ac4373c60cbded0a604a9d4cd2f3df92997264a4
Alien <128927380+alirezaw99@users.noreply.github.com>
2024-05-12 11:33:04 +0330

    some Bugs fixed
    -scrolling navbar on mobile fixed
    -extra white bar in mobile view fixed

--- Allin_Part/setting/dev.py
+++ Allin_Part/setting/dev.py
@@ -58,6 +58,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = 'media/'
 MEDIA_ROOT = 'media/'
 
+#security options
 CORS_REPLACE_HTTPS_REFERER      = False
 HOST_SCHEME                     = "http://"
 SECURE_PROXY_SSL_HEADER         = None
